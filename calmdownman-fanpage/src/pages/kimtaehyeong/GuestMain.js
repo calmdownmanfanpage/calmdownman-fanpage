de800124b31b65e962f5097fe102e34ac29aa9f6
taehyeong <xogud5053@hanmail.net>
2023-10-12 21:08:38 +0900

    add hover,flex

--- calmdownman-fanpage/src/pages/kimtaehyeong/GuestMain.js
+++ calmdownman-fanpage/src/pages/kimtaehyeong/GuestMain.js
@@ -1,12 +1,18 @@
-import { GuestAll, GuestSmall, GuestLarge } from "./GuestWindow";
+import { GuestAll, GuestSmall } from "./GuestWindow";
 
 const GuestMain = () => {
   return (
     <div>
       <div>게스트관계도 메인페이지입니다.</div>
       <GuestAll>
-        <GuestSmall>마우스대기전</GuestSmall>
-        <GuestLarge>마우스댄후</GuestLarge>
+        <GuestSmall>궤도</GuestSmall>
+        <GuestSmall>곽민수</GuestSmall>
+        <GuestSmall>통닭천사</GuestSmall>
+        <GuestSmall>매직박</GuestSmall>
+        <GuestSmall>김풍</GuestSmall>
+        <GuestSmall>주우재</GuestSmall>
+        <GuestSmall>곽튜브</GuestSmall>
+        <GuestSmall>박정민</GuestSmall>
       </GuestAll>
     </div>
   );
--- calmdownman-fanpage/src/pages/kimtaehyeong/GuestWindow.js
+++ calmdownman-fanpage/src/pages/kimtaehyeong/GuestWindow.js
@@ -2,22 +2,31 @@ import styled, { css } from "styled-components";
 
 const GuestAll = styled.div`
   background-color: skyblue;
-  width: 150px;
-  height: 200px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 30px;
 `;
 
 const GuestSmall = styled.div`
   background-color: blue;
-  width: 40px;
-  height: 40px;
-  border-radius: 50px;
+  color: white;
+  font-size: 20px;
+  width: 100px;
+  height: 100px;
+  border-radius: 100px;
+  transition: 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 20px;
+  &:hover {
+    background-color: green;
+    width: 110px;
+    height: 110px;
+    border-radius: 120px;
+    transition: 0.5s;
+  }
 `;
 
-const GuestLarge = styled.div`
-  background-color: green;
-  width: 50px;
-  height: 50px;
-  border-radius: 50px;
-`;
-
-export { GuestAll, GuestSmall, GuestLarge };
+export { GuestAll, GuestSmall };
