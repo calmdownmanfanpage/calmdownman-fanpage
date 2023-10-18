2e09a44aa1ff721817f8e042797e2d56d41f422a
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-10-18 19:31:50 +0900

    docs: delete file

--- calmdownman-fanpage/src/pages/choonghyun/Hello5.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Hello5(props) {
-  return <div> <img src="https://image-comic.pstatic.net/webtoon/687127/thumbnail/thumbnail_IMAG21_4063431270715437410.jpg" alt ></img> {props.name}</div>
-}
-
-export default Hello5;
