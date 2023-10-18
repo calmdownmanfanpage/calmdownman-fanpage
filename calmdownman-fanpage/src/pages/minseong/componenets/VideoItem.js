64f18f79941f8613312d9e839bb2d516ee7b1f9d
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-10-18 17:31:02 +0900

    feat: 영상 link 연결 컴포넌트

--- calmdownman-fanpage/src/pages/minseong/componenets/VideoItem.js
+++ calmdownman-fanpage/src/pages/minseong/componenets/VideoItem.js
@@ -1,28 +1,5 @@
-import styled from "styled-components";
-
-const StyledItemContainer = styled.div`
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-const StyledThumbnail = styled.div`
-  background-image: url(${(props) => props.thumbnail});
-  width: 200px;
-  height: 100px;
-  background-repeat: no-repeat;
-  background-size: contain;
-`;
-
-const VideoItem = ({ info }) => {
-  return (
-    <>
-      <StyledItemContainer>
-        <StyledThumbnail thumbnail={info.snippet.thumbnails.default.url} />
-      </StyledItemContainer>
-    </>
-  );
+const VideoItem = () => {
+  return <></>;
 };
 
 export default VideoItem;
