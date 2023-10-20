9baba890ce22b4bd6bda517aa5448d2824978590
Rosie <deresa0223@gmail.com>
2023-10-20 18:11:23 +0900

    feat : 캘린더 컴포넌트 분리, 캘린더 css 변경

--- calmdownman-fanpage/src/pages/soyoun/Calendar.js
+++ calmdownman-fanpage/src/pages/soyoun/Calendar.js
@@ -1,4 +1,4 @@
-// import styled, { css } from "styled-components";
+import styled, { css } from "styled-components";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
@@ -7,30 +7,107 @@ const events = [{ title: "Meeting", start: new Date() }];
 function Calendar() {
   return (
     <>
-        <FullCalendar
-          plugins={[dayGridPlugin]}
-          initialView="dayGridMonth"
-          weekends={true}
-          events={[
-          { 
-            title: "2주차 불침번", 
-            date: "2023-10-13"
-          },
-          {
-            title: "3주차 불침번",
-            date: "2023-10-16" 
-          },
-          
-        ]}
-          eventContent={renderEventContent}
-        />
-          
+      <StyledFullcalendarContainer>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            weekends={true}
+            locale={'ko'}
+            headerToolbar={
+                {
+                  start: 'today', 
+                  center: 'title',
+                  end: 'prev,next' 
+              }
+            }
+            events={[
+            { 
+              title: "2주차 불침번", 
+              date: "2023-10-13"
+            },
+            {
+              title: "3주차 불침번",
+              date: "2023-10-16" 
+            },
+            
+          ]}
+            eventContent={renderEventContent}
+          />
+      </StyledFullcalendarContainer>
     </>
   );
 }
 
 //Styled Component
+const StyledFullcalendarContainer = styled.div`
+  // toolbar container
+  .fc .fc-toolbar.fc-header-toolbar {
+    margin: 0;
+    padding: 0 40px;
+    background-color: #356eff;
+    height: 63px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 29px;
+    color: white;
+    border-radius: 20px 20px 0px 0px;
+  }
 
+  // toolbar 버튼
+  .fc .fc-button-primary {
+    background-color: transparent;
+    border: none;
+
+    span {
+      font-weight: 500;
+      font-size: 28px;
+    }
+
+    :hover {
+      background-color: transparent;
+    }
+  }
+
+  // 요일 부분
+  .fc-theme-standard th {
+    height: 32px;
+    padding-top: 3.5px;
+    background: #e5edff;
+    border: 1px solid #dddee0;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 19px;
+    color: #7b7b7b;
+  }
+
+  // 오늘 날짜 배경색
+  .fc .fc-daygrid-day.fc-day-today {
+    background-color: #fff8bd;
+    color: #356eff;
+  }
+
+  // 날짜별 그리드
+  .fc .fc-daygrid-day-frame {
+    padding: 10px;
+  }
+
+  // 날짜  ex) 2일
+  .fc .fc-daygrid-day-top {
+    flex-direction: row;
+    margin-bottom: 3px;
+  }
+
+  // 각 이벤트 요소
+  .fc-event {
+    cursor: pointer;
+    padding: 5px 8px;
+    margin-bottom: 5px;
+    border-radius: 4px;
+    font-weight: 500;
+    font-size: 14px;
+  }
+
+`;
 
 // a custom render function
 function renderEventContent(eventInfo) {
--- calmdownman-fanpage/src/pages/soyoun/Scheduler.js
+++ calmdownman-fanpage/src/pages/soyoun/Scheduler.js
@@ -1,13 +1,8 @@
-import { Link } from 'react-router-dom'
+
 import styled, { css } from "styled-components";
 import Bg from "./img/bg.png";
-import Insta from "./img/insta.png";
-import Twitch from "./img/twitch.png";
-import YoutubeLogo from "./img/youtubeLogo.png";
-import PlusLogo from "./img/plusLogo.png";
-import OriginalLogo from "./img/originalLogo.png";
-import CafeLogo from "./img/cafeLogo.png";
 import Calendar from "./Calendar";
+import Linklist from "./Linklist";
 
 const Scheduler = () => {
   return (
@@ -22,70 +17,7 @@ const Scheduler = () => {
             <Calendar />
           </CalendarArea>
           <InfoArea>
-            <Linklist>
-              <ul>
-                <li>
-                  <span>침착맨 생방송</span>
-                  <div>
-                    <Link to="https://www.twitch.tv/zilioner">
-                      <img
-                          src={Twitch}
-                          alt="트위치생방송로고"
-                      />
-                    </Link>
-                  </div>
-                </li>
-                <li>
-                  <span>침착맨 유튜브</span>
-                  <div>
-                  <Link to="https://www.youtube.com/@ChimChakMan_Official/featured">
-                      <img
-                          src={YoutubeLogo}
-                          alt="유튜브로고"
-                      />
-                    </Link>
-                  </div>
-                  <div>
-                  <Link to="https://www.youtube.com/@ChimChakMan_Plus">
-                      <img
-                          src={PlusLogo}
-                          alt="유튜브쇼츠로고"
-                      />
-                    </Link>
-                  </div>
-                  <div>
-                  <Link to="https://www.yhttps://www.youtube.com/@ChimChakMan_Dataoutube.com/@ChimChakMan_Plus">
-                      <img
-                          src={OriginalLogo}
-                          alt="원본박물관로고"
-                      />
-                    </Link>
-                  </div>
-                </li>
-                <li>
-                  <span>침착맨 인스타</span>
-                  <div>
-                    <Link to="https://www.instagram.com/dangerousmanleebyeonggeon/">
-                      <img
-                          src={Insta}
-                          alt="인스타그램로고"
-                      />
-                      </Link>
-                  </div>
-                </li>
-                <li>
-                  <span>참하하</span>
-                  <div>
-                    <Link to="https://chimhaha.net/">
-                      <img
-                          src={CafeLogo}
-                          alt="침하하카페로고"
-                      />
-                    </Link>
-                  </div>
-                </li>
-              </ul>
-            </Linklist>
+              <Linklist></Linklist>
           </InfoArea>
         </Container>
         {/* 
@@ -142,43 +74,7 @@ const InfoArea = styled.div`
   }
 `;
 
-const Linklist = styled.div`
-    ul{
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        @media screen and (min-width: 834px) {
-            flex-direction: column;
-        }
-    } 
-    li{
-        font-size: 30px;
-        font-family: Yeon Sung;
-        width: 50%;
-        padding: 10px 10px;
-        text-align: center;
-        & + li{
-        }
-        @media screen and (min-width: 834px) {
-            margin-top: 30px;
-            width: 100%;
-        }
-    }
-    span{
-      display: block;
-      & + div {
-        padding-top: 20px;
-      }
-    }
-    div{
-        display: inline-block;
-        width: 50px;
-        margin-right: 20px;
-    }
-    img{
-        width: 100%;
-    }
-`;
+
 
 
 export default Scheduler;
