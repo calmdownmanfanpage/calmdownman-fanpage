7b071bce76ed729bf6f915a29f1d9786e546aaea
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-10-20 18:09:58 +0900

    feat: 해당 videoId에 따른 영상 재생 기능 추가

--- calmdownman-fanpage/src/pages/minseong/componenets/VideoItem.js
+++ calmdownman-fanpage/src/pages/minseong/componenets/VideoItem.js
@@ -1,9 +1,31 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const VideoItem = () => {
   const { videoId } = useParams();
 
-  return <></>;
+  const StyledWrapper = styled.div`
+    width: 100%;
+    margin-top: 10px;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+  `;
+  const StyledIframe = styled.iframe`
+    width: 1024px;
+    height: 600px;
+    border-radius: 20px;
+  `;
+  return (
+    <>
+      <StyledWrapper>
+        <StyledIframe
+          title={videoId}
+          src={`https://www.youtube.com/embed/${videoId}?showinfo=0&enablejsapi=1&origin=http://localhost:3000`}
+        />
+      </StyledWrapper>
+    </>
+  );
 };
 
 export default VideoItem;
