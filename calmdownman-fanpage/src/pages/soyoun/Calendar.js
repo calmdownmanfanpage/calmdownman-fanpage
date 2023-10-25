968e172b9c24760d71b0c69415d5674bfe8ce4cf
soyounRoh <deresa0223@gmail.com>
2023-10-25 17:53:38 +0900

    commit

--- calmdownman-fanpage/src/pages/soyoun/Calendar.js
+++ calmdownman-fanpage/src/pages/soyoun/Calendar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { formatDate } from '@fullcalendar/core'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -31,18 +31,15 @@ class Calendar extends React.Component {
             selectMirror={true}
             dayMaxEvents={true}
             weekends={true}
-            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
             select={this.handleDateSelect}
             eventContent={renderEventContent} // custom render function
             eventClick={this.handleEventClick}
             eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            
-            eventAdd={function(){
-
-            }}
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            
+            events={localStorage.getItem('key')}
+            // eventAdd={}
+            // eventChange={function(){}}
+            // eventRemove={function(){}}
           />
         </div>
       </div>
@@ -51,23 +48,29 @@ class Calendar extends React.Component {
 
   
   handleDateSelect = (selectInfo) => {
-    let title = prompt('Please enter a new title for your event');
     let calendarApi = selectInfo.view.calendar;
-
-    calendarApi.unselect() // clear date selection
+    let title = prompt('Please enter a new title for your event');
+    // let title = localStorage.setItem(prompt('Please enter a new title for your event'), selectInfo.view.calendar);
+    // const dataArray = JSON.parse(localStorage.getItem(calendarApi)) || [];
+    
+    // calendarApi.unselect() // clear date selection
 
     if (title) {
-      calendarApi.addEvent({
+      const newEvent = calendarApi.addEvent({
         id: createEventId(),
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
         allDay: selectInfo.allDay
-      })
+      });
 
-      return INITIAL_EVENTS.push(calendarApi);
+      const eventList = localStorage.setItem(title, JSON.stringify(newEvent));
+     
+      // return INITIAL_EVENTS.push(newEvent);
     }
   }
+
+
 
   handleEventClick = (clickInfo) => {
     if (alert(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
