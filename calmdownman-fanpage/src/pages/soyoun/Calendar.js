fd26c59c954397f42c5f5956187cf33b488dba6c
soyounRoh <deresa0223@gmail.com>
2023-10-27 14:55:41 +0900

    code : class > 함수형으로 변경

--- calmdownman-fanpage/src/pages/soyoun/Calendar.js
+++ calmdownman-fanpage/src/pages/soyoun/Calendar.js
@@ -1,59 +1,18 @@
-import React, { PureComponent } from 'react'
-import { formatDate } from '@fullcalendar/core'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './event-utils'
+import React, { useEffect, useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { createEventId } from './event-utils';
 
-class Calendar extends React.Component {
-
-  state = {
-    weekendsVisible: true,
-    currentEvents: []
-  }
-
-  render() {
-    return (
-      <div className='demo-app'>
-        <div className='demo-app-main'>
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            initialView='dayGridMonth'
-            locale={"ko"}
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={true}
-            // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-            select={this.handleDateSelect}
-            eventContent={renderEventContent} // custom render function
-            eventClick={this.handleEventClick}
-            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            events={localStorage.getItem('key')}
-            // eventAdd={}
-            // eventChange={function(){}}
-            // eventRemove={function(){}}
-          />
-        </div>
-      </div>
-    )
-  }
-
+function Calendar(props) {
+  const [weekendsVisible, setWeekendsVisible] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState([]);
   
-  handleDateSelect = (selectInfo) => {
+
+  const handleDateSelect = (selectInfo) => {
     let calendarApi = selectInfo.view.calendar;
     let title = prompt('Please enter a new title for your event');
-    // let title = localStorage.setItem(prompt('Please enter a new title for your event'), selectInfo.view.calendar);
-    // const dataArray = JSON.parse(localStorage.getItem(calendarApi)) || [];
-    
-    // calendarApi.unselect() // clear date selection
 
     if (title) {
       const newEvent = calendarApi.addEvent({
@@ -61,29 +20,49 @@ class Calendar extends React.Component {
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
-        allDay: selectInfo.allDay
+        allDay: selectInfo.allDay,
       });
 
       const eventList = localStorage.setItem(title, JSON.stringify(newEvent));
-     
-      // return INITIAL_EVENTS.push(newEvent);
     }
-  }
+  };
 
-
-
-  handleEventClick = (clickInfo) => {
-    if (alert(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove()
+  const handleEventClick = (clickInfo) => {
+    if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
     }
-  }
+  };
 
-  handleEvents = (events) => {
-    this.setState({
-      currentEvents: events
-    })
-  }
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
+  };
 
+  return (
+    <div className='demo-app'>
+      <div className='demo-app-main'>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          initialView='dayGridMonth'
+          locale={"ko"}
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={weekendsVisible}
+          select={handleDateSelect}
+          eventContent={renderEventContent} // custom render function
+          eventClick={handleEventClick}
+          eventsSet={handleEvents} // called after eents are initialized/added/changed/removed
+          events={props.INITIAL_EVENTS}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Calendar;
@@ -94,15 +73,14 @@ function renderEventContent(eventInfo) {
       <b>{eventInfo.timeText}</b>
       <i>{eventInfo.event.title}</i>
     </>
-  )
+  );
 }
 
 function renderSidebarEvent(event) {
   return (
     <li key={event.id}>
-      <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
-      <i>{event.title}</i>
+      {/* <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+      <i>{event.title}</i> */}
     </li>
-  )
+  );
 }
-
